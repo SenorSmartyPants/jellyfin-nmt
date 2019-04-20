f603599c641820083459768b37fbaeebc34e5702
SenorSmartyPants <senorsmartypants@gmail.com>
2019-04-20 13:05:36 -0500

    Header, poster grid. lots of stuff

--- latest.php
+++ latest.php
@@ -4,7 +4,10 @@ include 'secrets.php';
 include 'utils.php';
 
 $GroupItems = "true";
-$Limit = 21;
+$Limit = 27;
+
+//latest tv ==
+//&IncludeItemTypes=episode,series
 
 //skin options
 $nbThumbnailsPerPage = 27;
@@ -55,9 +58,18 @@ function printHeadEtc()
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>Moviejukebox</title>
 
-    <body bgproperties="fixed" onloadset="1" FOCUSTEXT="#FFFFFF" focuscolor="transparent" onload="initpage()"
-    background="<?= $jukebox_url ?>/pictures/wall/background1.jpg">
- 
+        <style>
+            .indexname {
+                font-size: 23pt;
+                font-weight: normal;
+                color: #8e8e8e;
+            }
+        </style>
+
+    </head>
+
+    <body bgproperties="fixed" onloadset="1" FOCUSTEXT="#FFFFFF" focuscolor="transparent" onload="initpage()" background="<?= $jukebox_url ?>pictures/wall/background1.jpg">
+
     <?php
 }
 
@@ -70,18 +82,18 @@ function printFooter()
 <?php
 }
 
-function printPosterTable()
+function printPosterTable($items)
 {
-    global $api_url, $user_id, $api_key, $GroupItems, $Limit;
-    ;
     global $nbThumbnailsPerLine;
     //set table is centered
     $align = "left";
     ?>
-    <table class="movies" border="0" cellpadding="0" cellspacing="4" align="<?= $align ?>" >
-<?php
-/*
+    <table class="movies" border="0" cellpadding="0" cellspacing="4" align="<?= $align ?>">
+        <?php
+        /*
     <xsl:for-each select="library/movies/movie[position() mod $nbCols = 1]"> //selects first item in row
+    //here position is row number
+    //gap is # items already displayed on previous rows 
       <tr>
         <xsl:apply-templates
              select=".|following-sibling::movie[position() &lt; $nbCols]">
@@ -93,64 +105,81 @@ function printPosterTable()
       </tr>
     </xsl:for-each>
     */
-    $url = $api_url . "/Users/" . $user_id .
-    "/Items/Latest?&GroupItems=" . $GroupItems .
-    "&Limit=" . $Limit .
-    "&api_key=" . $api_key;
 
-    echo "<a href=\"" . $url . "\">url</a><br/>";
+        foreach ($items as $key => $item) {
+            //first item in row
+            if (isStartOfRow($key)) {
+                echo "<tr>";
+            }
+            switch ($item->Type) {
+                case "Episode":
+                    $menuItem = parseEpisode($item);
+                    break;
+                case "Series":
+                    $menuItem = parseSeries($item);
+                    break;
+                case "Movie":
+                    $menuItem = parseMovie($item);
+                    break;
+            }
+            //printMenuItem($menuItem);
+            printPostTD($menuItem, 0, $key);
 
-
-    $contents = file_get_contents($url);
-    $latest = json_decode($contents);
-
-    //gap is ?
-    foreach ($latest as $key => $item) {
-        if ($key % $nbThumbnailsPerLine == 0) 
-        {
-            echo "<tr>";
+            //last item in row
+            if (isEndOfRow($key)) {
+                echo "</tr>";
+            }
         }
-        switch ($item->Type) {
-            case "Episode":
-                $menuItem = parseEpisode($item);
-                break;
-            case "Series":
-                $menuItem = parseSeries($item);  
-                break;
-            case "Movie":
-                $menuItem = parseMovie($item);
-                break;
-        }
-        //printMenuItem($menuItem);
-        printPostTD($menuItem, 0, $key);
-        if ($key % $nbThumbnailsPerLine == $nbThumbnailsPerLine - 1) 
-        {
-            echo "</tr>";
-        }
-    }
-    ?>
+        ?>
     </table>
-<?php 
+<?php
+}
+
+function isStartOfRow($position)
+{
+    global $nbThumbnailsPerLine;
+    return ($position % $nbThumbnailsPerLine == 0);
+}
+
+function isEndOfRow($position)
+{
+    global $nbThumbnailsPerLine;
+    return ($position % $nbThumbnailsPerLine == $nbThumbnailsPerLine - 1);
 }
 
 //gap is for skipping rows, in sets on the bottom
 function printPostTD($menuItem, $gap, $position)
 {
     global $api_url, $jukebox_url, $popupWidth, $popupHeight;
-    global $thumbnailsWidth,$thumbnailsHeight;
-    $placement = $position + $gap;
+    global $thumbnailsWidth, $thumbnailsHeight;
+    global $nbThumbnailsPerPage, $nbThumbnailsPerLine;
+    $placement = $position + $gap + 1; //$position is zero based
     ?>
     <td align="center">
-        <a href="<?= $jukebox_url . $menuItem->DetailBaseURL ?>" onclick="return prompter('TV-14 hardcode')" 
-        TVID="<?= $placement ?>" name="<?= $placement ?>" 
-        onmouseover="show(<?= $placement ?>, 'do I even use this?')" 
-        onmouseout="hide(<?= $placement ?>, 'or this?')" 
-        onfocus="show(<?= $placement ?>, 'do I even use this?')" 
-        onblur="hide(<?= $placement ?>, 'or this?')" 
-        >
-        <?php
-                                                                                                                                                                                                                            
-        /*   
+        <a href="<?= $jukebox_url . $menuItem->DetailBaseURL ?>" onclick="return prompter('TV-14 hardcode')" TVID="<?= $placement ?>" name="<?= $placement ?>" onmouseover="show(<?= $placement ?>, 'do I even use this?')" onmouseout="hide(<?= $placement ?>, 'or this?')" onfocus="show(<?= $placement ?>, 'do I even use this?')" onblur="hide(<?= $placement ?>, 'or this?')" 
+<?php
+
+    //start of row
+    if (isStartOfRow($placement - 1)) {
+        if ($placement == 1) {
+            echo "onkeyrightset=\"pgupload\"";
+        } else {
+            echo "onkeyleftset=\"" . ($placement - 1) . "\"";
+        }
+    }
+
+    //end of row
+    if (isEndOfRow($placement - 1)) {
+        if ($placement != $nbThumbnailsPerPage) {
+            echo "onkeyrightset=\"" . ($placement + 1) . "\"";
+        } else {
+            echo "onkeyrightset=\"pgdnload\"";
+        }
+    }
+
+
+
+/*  TODO: is this anything I want to keep? 
                 <xsl:choose>
                     <xsl:when test="count(files/file) > 1">
                         <xsl:call-template name="search-and-replace">
@@ -207,7 +236,7 @@ function printPostTD($menuItem, $gap, $position)
 
 
 
-//TODO: this manages keyset navigation 
+//TODO: this manages keyset navigation . Top and bottom rows
             <xsl:if test="$lastIndex != 0">
                 <xsl:if test="$gap=0 and $currentIndex != 0 and $FilterBar='true'">
                     <xsl:attribute name="onkeyupset">left</xsl:attribute>
@@ -219,34 +248,66 @@ function printPostTD($menuItem, $gap, $position)
                     <xsl:attribute name="onkeyupset">moviestv</xsl:attribute>
                 </xsl:if>
 
-                <xsl:if test="position()+$gap = 1">
-                    <xsl:attribute name="onkeyleftset">pgupload</xsl:attribute>
-                </xsl:if>
-
-                <xsl:if test="position()+$gap = 9">
-                    <xsl:attribute name="onkeyrightset">10</xsl:attribute>
-                </xsl:if>
-                <xsl:if test="position()+$gap = 10">
-                    <xsl:attribute name="onkeyleftset">9</xsl:attribute>
-                </xsl:if>
-                <xsl:if test="position()+$gap = 18">
-                    <xsl:attribute name="onkeyrightset">19</xsl:attribute>
-                </xsl:if>
-                <xsl:if test="position()+$gap = 19">
-                    <xsl:attribute name="onkeyleftset">18</xsl:attribute>
-                </xsl:if>
-                <xsl:if test="position()+$gap = 27">
-                    <xsl:attribute name="onkeyrightset">pgdnload</xsl:attribute>
-                </xsl:if>
-
                 <xsl:if test="$gap=$lastGap and $currentIndex != $lastIndex">
                     <xsl:attribute name="onkeydownset">pgdnload</xsl:attribute>
                 </xsl:if>
             </xsl:if>                
         */
-        ?> 
-        <img src="<?= $api_url ?>/Items/<?= $menuItem->PosterID ?>/Images/Primary?UnplayedCount=<?= $menuItem->UnplayedCount ?>&maxHeight=<?= $popupHeight ?>&maxWidth=<?= $popupWidth ?>" width="<?= $thumbnailsWidth ?>" height="<?= $thumbnailsHeight ?>" onfocussrc="pictures/wall/transparent.png"/></a>
+                                                                                                                                                                                                                                                                                                                                                                                    ?>>
+            <img src="<?= $api_url ?>/Items/<?= $menuItem->PosterID ?>/Images/Primary?UnplayedCount=<?= $menuItem->UnplayedCount ?>&maxHeight=<?= $popupHeight ?>&maxWidth=<?= $popupWidth ?>" width="<?= $thumbnailsWidth ?>" height="<?= $thumbnailsHeight ?>" onfocussrc="pictures/wall/transparent.png" /></a>
     </td>
+<?php
+}
+
+function printNavbar()
+{
+    global $jukebox_url;
+
+    //83 final height for navbar in original
+    $imagePadHeight = 83; //56 in original layout
+    ?>
+    <table class="main" border="0" cellpadding="0" cellspacing="0">
+        <tr valign="top">
+            <td align="left" valign="top" height="<?= $imagePadHeight ?>" width="1"><img src="<?= $jukebox_url ?>pictures/detail/1x688.png" height="<?= $imagePadHeight ?>" /></td>
+            <td class="indexname" id="indexmenuleft" align="left" valign="top" height="<?= $imagePadHeight ?>" width="265">
+                Latest
+            </td>
+            <td id="indexmenuright" align="right">place holding</td>
+        </tr>
+    </table>
+<?php
+}
+
+function printTitleTable()
+{
+    ?>
+    <table border="0" cellpadding="10" cellspacing="0" width="100%" align="center">
+        <!--<xsl:if test="$index-titlebackground = 'true'"><xsl:attribute name="background">pictures/dim/custom_tvtitle_dim.png</xsl:attribute></xsl:if>-->
+        <tr>
+            <td width="25%"></td>
+            <td width="50%" align="center" id="title" valign="top">&#160;</td>
+
+
+            <td width="25%" align="right" id="page" valign="top">
+                <!--<xsl:value-of select="$Page"/>&#160;
+	
+	        <xsl:if test="$ForComputer='false'"><xsl:value-of select="$currentIndex"/></xsl:if>
+	        <xsl:if test="$ForComputer='true'">
+		        <a>
+			        <xsl:attribute name="href">
+			        <xsl:value-of select="library/category[@current='true']/index[@current='true']/@previous" />.html</xsl:attribute>
+			        <xsl:value-of select="$currentIndex"/>
+		        </a>
+	        </xsl:if>
+	
+	        &#160;<xsl:value-of select="$OutOf"/>&#160;<a onfocusload=""><xsl:attribute name="href"><xsl:value-of select="library/category[@current='true']/index[@current='true']/@next" />.html</xsl:attribute><xsl:value-of select="$lastIndex" /></a>-->
+            </td>
+        </tr>
+        <tr>
+            <td>&#160;</td>
+            <td align="right" id="year" valign="top">&#160;</td>
+        </tr>
+    </table>
 <?php
 }
 
@@ -254,7 +315,13 @@ function printPostTD($menuItem, $gap, $position)
 
 printHeadEtc();
 
-printPosterTable();
+printNavbar();
+
+printPosterTable(getLatest($Limit));
+
+printTitleTable();
 
 printFooter();
+
+echo "<div class=\"indexname\">API call count = $apiCallCount</div>";
 ?>
