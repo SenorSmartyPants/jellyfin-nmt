f3a7a0237fdd3d175875d53005114dc8679d630f
SenorSmartyPants <senorsmartypants@gmail.com>
2020-01-19 19:14:18 -0600

    Top Bar working

--- Season.php
+++ Season.php
@@ -1,18 +1,51 @@
 <?
 /*
 no tv rating in JF episode data?
+TODO: support season banner. But most in JF don't have
+    Banner(doesn't inherit):
+
+        <br />
+    Season
+    Overview and OfficialRating - missing, pull separately?
+    <br />
+    episodes support ParentalRating, but not populated in JF
+    <br />
+    Support watched. Check skin to see <?= $item->UserData->Played ?>
+
 
 */
 include_once 'data.php';
 include_once 'listings.php';
+include_once 'templates.php';
 
 $titleTruncate = 34;
+$ShowAudioCodec = true;
+$ShowContainer = true;
+$ShowVideoOutput = true;
+$star_rating = true;
+$tvNumberRating = false;
 
-printHeadEtc();
+$id = $_GET["id"];
 
-render();
+$itemsAndCount = getUsersItems(null, "Path,Overview,Height,Width,MediaSources,ProviderIds", null, $id);
+$items = $itemsAndCount->Items;
+$item = $items[0];
 
-printFooter();
+$firstSource = $item->MediaSources[0];
+if ($firstSource) {
+    foreach ($firstSource->MediaStreams as $mediastream) {
+        if ($mediastream->Type == 'Video' && $mediastream->IsDefault) {
+            $videoStream = $mediastream;
+        }
+    }
+    $audioStream = $firstSource->MediaStreams[$firstSource->DefaultAudioStreamIndex];
+    //can have subs without a default
+    $subtitleStream = $firstSource->MediaStreams[$firstSource->DefaultSubtitleStreamIndex];
+}
+
+printSeasonHeadEtc();
+
+printSeasonFooter();
 
 function formatCast($cast)
 {
@@ -71,142 +104,125 @@ function renderEpisodeHTML($episode, $indexInList)
 <?
 }
 
-function render()
+function printSeasonHeadEtc($onloadset = null)
 {
-    $id = $_GET["id"];
+    global $item;
 
-    $itemsAndCount = getUsersItems(null, "Path,Overview,Height,Width,MediaSources,ProviderIds", null, $id);
-    $items = $itemsAndCount->Items;
-    $item = $items[0];
+    global $theme_css, $indexStyle;
+    $onloadset = $onloadset ?? "1";
+    ?>
+    <html>
 
-    renderEpisodeJS($item);
+    <head>
+        <link rel="shortcut icon" href="<?= getFavIconURL() ?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Jellyfin NMT</title>
 
-    renderEpisodeHTML($item, 1);
-
-    $firstSource = $item->MediaSources[0];
-    if ($firstSource) {
-        foreach ($firstSource->MediaStreams as $mediastream) {
-            if ($mediastream->Type == 'Video' && $mediastream->IsDefault) {
-                $videoStream = $mediastream;
-            }
-        }
-        $audioStream = $firstSource->MediaStreams[$firstSource->DefaultAudioStreamIndex];
-        //can have subs without a default
-        $subtitleStream = $firstSource->MediaStreams[$firstSource->DefaultSubtitleStreamIndex];
-    }
-    $CC = $item->HasSubtitles;
-
-
-    $seasonYear = $item->ProductionYear;
-    //use gmdate because PremiereDate usually is only a date, time is not significant
-    //don't do localtime translation
-    if ($item->PremiereDate) {
-        $date = gmdate("n/j/Y", strtotime($item->PremiereDate));
-    }
-
-
-    $added = date("n/j/Y g:i A", strtotime($item->DateCreated));
-
+<?
+        if (isset($indexStyle) && null !== $indexStyle->cssFile()) {
 ?>
+        <!-- don't add any styles before the following. JS show/hide code depends on this these being first -->
+        <link rel="StyleSheet" type="text/css" href="<?= $indexStyle->cssFile() ?>"/>
+<?
+        }
+?>
+        <link rel="StyleSheet" type="text/css" href="css/base.css" />
+        <link rel="StyleSheet" type="text/css" href="css/themes/<?= $theme_css ?>" />
 
-    <h1>
-        S<?= $item->ParentIndexNumber ?>
-        <?= $seasonYear ?>
+        <style>
+/*Sabish TVMod V2 - override*/
+.test {color: c6cace; font-weight: normal; font-size: 32pt;!important}
+#divEpisodeImgBackSabish {top: 405px; left: 20px;!important}
+#divEpisodeImgSabish {top: 417px; left: 31px;!important}
+#divEpisodeCertification {top: 597px; left: 161px;!important}
+.tvseason {color: c6cace; font-weight: normal; font-size: 32pt;!important}
+.tvyear {color: 868686; font-weight: normal; font-size: 32pt;!important}
+.tveptitle {font-size:26pt; font-weight: bold; color:#FFFFFF}
 
-        <?= $audioStream->Codec ?>
-        <?= $audioStream->ChannelLayout ?>
+.tveptitle24 {font-size:24pt; font-weight: bold; color:#FFFFFF}
+.tveptitle22 {font-size:22pt; font-weight: bold; color:#FFFFFF}
+.tveptitle20 {font-size:20pt; font-weight: bold; color:#FFFFFF}
+.tveptitle18 {font-size:18pt; font-weight: bold; color:#FFFFFF}
+.tveptitle16 {font-size:16pt; font-weight: bold; color:#FFFFFF}
+.tveptitle14 {font-size:14pt; font-weight: bold; color:#FFFFFF}
 
-        <?= $firstSource->Container ?>
-        <?= $videoStream->Codec ?>
-        <?= $videoStream->Height ?>
+.tvplot {color: ececec; font-weight: normal; font-size: 13pt;!important}
+.TvLink {color: 868686; font-size: 12pt; font-weight: bold;!important}
+.TvLink11 {color: 868686; font-size: 11pt; font-weight: bold;!important}
+.rating {color: ffffff; font-weight: 400; font-size: 18pt;!important}
 
-        <?= $item->CommunityRating ?>*
-    </h1>
+</style>
 
-    <br />
-    Season
-    Overview and OfficialRating - missing, pull separately?
-    <br />
-    episodes support ParentalRating, but not populated in JF
-    <br />
-    Support watched. Check skin to see <?= $item->UserData->Played ?>
+        <script>
 
+        </script>
 
+    </head>
 
-    <table border="1">
+    <body bgproperties="fixed" onloadset="episode1" onload="initNew()" bgcolor="#000000" focustext="#FFFFFF" FOCUSCOLOR="transparent" 
+    <? if ($item->ParentBackdropImageTags[0]) { ?> background="<?= getImageURL($item->ParentBackdropItemId, 720, 1280, "Backdrop", null, null, $item->ParentBackdropImageTags[0]) ?>"> <? } ?>
+
+    <table height="656" width="1102" border="0" cellspacing="0" cellpadding="0" background="/New/Jukebox/pictures/sabishmod/tvbg-v2.png">
         <tr>
             <td valign="top">
-                <? if ($item->ImageTags->Primary) { ?><img src="<?= getImageURL($item->Id, 360, null, "Primary", null, null, $item->ImageTags->Primary) ?>" /> <? } ?>
-
-            </td>
-            <td>
-
-                <h1><?= $item->IndexNumber . '. ' . $item->Name ?></h1>
-                <br />
-
-                <?= $item->Overview ?><br /><br />
-
-
-            </td>
-        </tr>
-    </table>
-
-
-    Images:<br />
-    support season banner and backdrop. But most in JF don't have<br />
-
-    <? if ($item->ImageTags->Primary) { ?><img src="<?= getImageURL($item->Id, 200, null, "Primary", null, null, $item->ImageTags->Primary) ?>" /> <? } ?>
-    <br />
-    Parent Images:<br />
-
-    <!--no banners set for seasons yet -->
-    Banner(doesn't inherit):<br />
-    <? if ($item->SeriesId) { ?><img src="<?= getImageURL($item->SeriesId, 200, null, "Banner", null, null, null) ?>" /> <? } ?>
-    <br />
-    Backdrop(inherit):<br />
-    <? if ($item->ParentBackdropImageTags[0]) { ?><img src="<?= getImageURL($item->ParentBackdropItemId, 200, null, "Backdrop", null, null, $item->ParentBackdropImageTags[0]) ?>" /> <? } ?>
-
-    <br />
-    tvdb:<?= $item->ProviderIds->Tvdb ?><br />
-
-    <!--
- 
-    <h2>Some Media Info</h2>
-    
-    <br/>
-    <?= $videoStream->Type ?>:<br/>
-    DisplayTitle: <?= $videoStream->DisplayTitle ?><br/>
-    Codec: <?= $videoStream->Codec ?><br/>
-    <?= $videoStream->AspectRatio ?><br/>
-    <?= $videoStream->Width ?><br/>
-    <?= $videoStream->RealFrameRate ?><br/>
-    
-    
-    <br/>
-    <?= $audioStream->Type ?>:<br/>
-    DisplayTitle: <?= $audioStream->DisplayTitle ?><br/>
-    Title:<?= $audioStream->Title ?><br/> VB 3x01 for example<br/>
-    Codec: <?= $audioStream->Codec ?><br/>
-    <?= $audioStream->ChannelLayout ?><br/>
-    <?= $audioStream->Language ?><br/>
-    
-    <br/>
-    <? if ($subtitleStream) {
-    ?>
-    <?= $subtitleStream->Type ?>:
-    <?= $subtitleStream->DisplayTitle ?><br/>
-    <?= $subtitleStream->Codec ?><br/>
-    <?= $subtitleStream->Language ?><br/>
-    <?
-    }
-    ?>
-
-    Container: <?= $firstSource->Container ?><br/>
-    Path: <?= $item->Path ?><br/>
-    Size: <?= round($firstSource->Size / 1024 / 1024) ?> MB<br/>
-
--->
 <?
+    printTopBar();
 }
 
+function printTopBar()
+{
+    global $item, $videoStream, $audioStream, $firstSource;
+    global $ShowAudioCodec, $ShowContainer, $ShowVideoOutput, $star_rating, $tvNumberRating;
+?>
+    <table border="0" cellspacing="0" cellpadding="0">
+        <tr height="50" valign="bottom">
+            <td width="18"></td>
+            <td width="250">
+                <img width="244" height="45" src="<?= getImageURL($item->SeriesId, 45, 244, "Banner") ?>" />
+            </td>
+            <td width="30"></td>
+            <td align="center" valign="center" class="tvseason"><?= ($item->ParentIndexNumber > 0) ? "S" . $item->ParentIndexNumber : "Sp" ?></td>
+            <td width="20"></td>
+
+            <td align="center" valign="center" class="tvyear"><?= $item->ProductionYear ?></td>
+
+            <td width="50"></td>
+            <td valign="center" style="font-size: medium"><?= $ShowAudioCodec ? audioCodec($audioStream) : null ?><?= $ShowContainer ? container($firstSource->Container) : null ?><?= $ShowVideoOutput ? videoOutput($videoStream) : null ?></td>
+
+            <?= $item->ParentIndexNumber < 10 ? '<td width="90"></td>' : null ?> 
+            <?= ($item->ParentIndexNumber > 9 and $item->ParentIndexNumber < 100) ? '<td width="70"></td>' : null ?>
+            <?= ($item->ParentIndexNumber > 99 and $item->ParentIndexNumber < 1000) ? '<td width="50"></td>' : null ?>
+            <?= ($item->ParentIndexNumber > 999) ? '<td width="30"></td>' : null ?>
+            
+            <td align="right" valign="center" class="rating">						
+                <? 
+                if ($item->CommunityRating) 
+                {
+                    if ($star_rating) 
+                    { ?>
+                        <img hspace="10" vspace="10" src="/New/Jukebox/pictures/detail/rating_<?= round($item->CommunityRating)*10?>.png" >
+                        </img>
+                    <? }
+                    if ($tvNumberRating) 
+                    {
+                        echo "&nbsp;(" . $item->CommunityRating . "/10)"; 
+                    }
+                } ?>
+		    </td>			
+        </tr>
+    </table>
+<? 
+}
+
+function printSeasonFooter()
+{
+?>
+                </td>
+            </tr>
+        </table>  	
+    </body>
+
+    </html>
+<?
+}
 ?>
