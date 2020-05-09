7dceb1f4f0edf381c09b695d215247d7c40f4e59
SenorSmartyPants <senorsmartypants@gmail.com>
2020-05-09 14:38:57 -0500

    Don't encode single quotes

--- Season.php
+++ Season.php
@@ -147,7 +147,7 @@ function renderEpisodeHTML($episode, $indexInList, $episodeIndex)
         } else {
             $titleLine = sprintf('%02d', $episode->IndexNumber);
         }
-        $titleLine .= '. ' . ($episode->UserData->Played ? '* ' : '') . htmlentities(substr($episode->Name, 0, TITLETRUNCATE));
+        $titleLine .= '. ' . ($episode->UserData->Played ? '* ' : '') . htmlspecialchars(substr($episode->Name, 0, TITLETRUNCATE), ENT_NOQUOTES);
     }
 
     #region videoPlayLink setup
