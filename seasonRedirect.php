1f90b400fdd6639181fb2d261d4fecf44f12e3e9
SenorSmartyPants <senorsmartypants@gmail.com>
2020-08-09 17:45:17 -0500

    make seasonRedirect extend page, to get proper authentication

--- seasonRedirect.php
+++ seasonRedirect.php
@@ -1,20 +1,36 @@
 <?php
-include 'data.php';
+include_once 'page.php';
 
-$SeriesId = htmlspecialchars($_GET["SeriesId"]);
-$SeasonId = htmlspecialchars($_GET["SeasonId"]);
-$IndexNumber = htmlspecialchars($_GET["IndexNumber"]);
+class SeasonRedirectPage extends Page
+{
 
-//If seriesID is passed in then we don't know the season ID
-if ($SeriesId) {
-    $season = firstSeasonFromSeries($SeriesId);
-    $SeasonId = $season->Id;
+    public function __construct()
+    {
+        parent::__construct('');     
+    }
+
+    public function render()
+    {
+        $SeriesId = htmlspecialchars($_GET["SeriesId"]);
+        $SeasonId = htmlspecialchars($_GET["SeasonId"]);
+        $IndexNumber = htmlspecialchars($_GET["IndexNumber"]);
+        
+        //If seriesID is passed in then we don't know the season ID
+        if ($SeriesId) {
+            $season = firstSeasonFromSeries($SeriesId);
+            $SeasonId = $season->Id;
+        }
+        $DetailURL = "Season.php?id=" . $SeasonId . "&episode=" . $IndexNumber;
+
+        //301 redirect does not change window.location in NMT gaya browser
+        //this can cause images not to resolve if url is set dynamically in Javascript (using relative paths)
+        //using html refresh intead, which works in this case on gaya
+        //don't enclose url in single quotes, which break on gaya
+
+        echo '<meta http-equiv="REFRESH" content="0; url=' . $DetailURL . '" />';
+    }
 }
-$DetailURL = "Season.php?id=" . $SeasonId . "&episode=" . $IndexNumber;
 
-//301 redirect does not change window.location in NMT gaya browser
-//this can cause images not to resolve if url is set dynamically in Javascript (using relative paths)
-//using html refresh intead, which works in this case on gaya
-//don't enclose url in single quotes, which break on gaya
+$page = new SeasonRedirectPage();
+$page->render();
 ?>
-<meta http-equiv="REFRESH" content="0; url=<?= $DetailURL ?>" />
