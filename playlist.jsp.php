f4f7a0fd9b2d46bcdd0e37c3b1ac381e88d99599
SenorSmartyPants <senorsmartypants@gmail.com>
2021-04-07 16:08:35 -0500

    Playlist Support: Video/Audio playlist output

---
+++ playlist.jsp.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'utils.php';
+include_once 'page.php';
+
+// Playlist format documented here http://www.networkedmediatank.com/wiki/index.php/Playlist_File
+
+class PlaylistPage extends Page
+{
+    public $itemIDs;
+
+    public function __construct()
+    {
+        parent::__construct('');
+    }
+
+    protected function printItem($name, $url)
+    {
+        echo $name . '|0|0|' . $url . "|\n";
+    }
+
+    public function render()
+    {
+        header('Content-type: text/plain');
+
+        foreach ($this->itemIDs as $itemID) {
+            $item = getItem($itemID);
+            $this->printItem($item->Name, translatePathToNMT($item->Path));
+        }
+    }
+}
+
+$itemIDs = $_GET["itemIDs"];
+
+$page = new PlaylistPage();
+$page->itemIDs = explode(',', $_GET["itemIDs"]);
+$page->render();
