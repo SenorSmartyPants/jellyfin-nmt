776aa3d13ac8cba5ef556c1ca472460377053618
SenorSmartyPants <senorsmartypants@gmail.com>
2021-04-18 15:01:04 -0500

    Bump version to 0.4.0

--- data.php
+++ data.php
@@ -8,7 +8,7 @@ const USERSPATH = '/Users/';
 $apiCallCount = 0;
 
 const CLIENTNAME = 'Jellyfin-NMT';
-const CLIENTVERSION = '0.3.1';
+const CLIENTVERSION = '0.4.0';
 const POSTCONTENTTYPE = 'application/json';
 
 class Device
