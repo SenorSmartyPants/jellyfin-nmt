0420452a50928548b2d323240d913dab420563a1
SenorSmartyPants <senorsmartypants@gmail.com>
2022-02-24 02:22:33 -0600

    Rewrite home page to honor user display preferences
    Home section ordering
    Section ordering for Latest Items
    New links for Latest pages to support TopParentID
    Remove commented code

--- index.php
+++ index.php
@@ -43,44 +43,93 @@ setNumPagesAndIndexCount(count($items));
 
 class IndexPage extends ListingsPage
 {
-    private $continueWatching;
+    private $resume;
     //private $rewatching;
 
     public function __construct($title)
     {
-        parent::__construct($title, false);
-        //check if there are continueWatching items
-        $this->continueWatching = (getResume(1)->TotalRecordCount) > 0;
-        if ($this->continueWatching) {
-            $this->onloadset = 'continue';
-        } else {
-            $this->onloadset = 'nextup';
-        }
+        parent::__construct($title, false);        
+        //check if there are resume items
+        $this->resume = (getResume(1)->TotalRecordCount) > 0;
         //check if there are rewatching items
         //$this->rewatching = (getNextUp(1,0,true)->TotalRecordCount) > 0;
     }
 
     public function printContent()
     {
-        if ($this->continueWatching) {
-            echo '<a href="continueWatching.php" name="continue">Continue Watching ></a>&nbsp;';
-        } 
-    ?>
-        <a href="nextUp.php" name="nextup">Next Up ></a>
-    <?
-/*         if ($this->rewatching) {
-            echo '<a href="nextUp.php?rewatching" name="rewatching">Rewatching ></a>&nbsp;';
-        }  */
-    ?>
-        <br clear="all"/>
-        <a href="latest.php?type=<?= ItemType::EPISODE ?>">Latest TV Shows ></a>
-        <br clear="all"/>
-        <a href="latest.php?type=<?= ItemType::MOVIE ?>">Latest Movies ></a>
-        <br clear="all"/>
+        $nameAttr = ' name="1"';
+        $customPrefs = $this->displayPreferences->CustomPrefs;
+        for ($i=0; $i < 7; $i++) { 
+            $sectionname = 'homesection' . $i;
+            switch ($customPrefs->{$sectionname}) {
+                case 'resume':
+                    if ($this->resume) {
+                        echo '<a href="continueWatching.php"' . $nameAttr . '>Continue Watching ></a>&nbsp;';
+                        $nameAttr = null;
+                    }                     
+                    break;
+
+                case 'nextup':
+                    echo '<a href="nextUp.php"' . $nameAttr . '>Next Up ></a>';
+                    echo '<br clear="all"/>';
+                    $nameAttr = null;
+                    break;
+
+                case 'rewatching':
+                    if ($this->rewatching) {
+                        echo '<a href="nextUp.php?rewatching"' . $nameAttr . '>Rewatching ></a>&nbsp;';
+                        echo '<br clear="all"/>';
+                        $nameAttr = null;
+                    }
+                    break;                    
+                    
+                case 'latestmedia':
+                    $user = getUser();
+                    $latestExcludes = $user->Configuration->LatestItemsExcludes;
+                    foreach ($this->items as $view) {
+                        if ($view->CollectionType != CollectionType::BOXSETS 
+                            && $view->CollectionType != CollectionType::PLAYLISTS
+                            && !in_array($view->Id, $latestExcludes))
+                        {
+                            $cbp = new CategoryBrowseParams();
+
+                            $cbp->name = 'Latest';
+                            $cbp->topParentName = $view->Name;
+                            $cbp->topParentId = $view->Id;
+                            $cbp->collectionType = $view->CollectionType;
+                            
+                            ?>
+                            <a href="latest.php?<?= http_build_query($cbp) ?>" <?= $nameAttr ?>><?= $cbp->name . ' ' . $view->Name ?> ></a>
+                            <br clear="all"/>
+                            <?
+                            $nameAttr = null;     
+                        }    
+                    }
+                    break;
+                    
+                case 'librarybuttons':
+                case 'smalllibrarytiles':
+?>
         <a href="categoriesHTML.php">Categories ></a>
         <br clear="all"/>
-    <?
-        $this->printPosterTable($this->items);
+<?
+                    $this->printPosterTable($this->items);
+                    $nameAttr = null;
+                    break;                      
+
+                # Not supported sections
+                case 'resumeaudio':
+                case 'activerecordings':
+                case 'livetv':
+                case 'none':
+                    break;
+
+                default:
+                    # Catch new sections, just display name
+                    echo 'New section to support: ' . $customPrefs->{$sectionname} . '<br/>';
+                    break;
+            }
+        }
     }
 }
 
