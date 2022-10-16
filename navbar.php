3a7f84381c58801f4e8769de8a3394fb0d9f23c2
SenorSmartyPants <senorsmartypants@gmail.com>
2022-10-16 12:34:15 -0500

    Change downset to 0
    This will go to first in grid

--- navbar.php
+++ navbar.php
@@ -16,7 +16,7 @@ class Navbar
             <tr valign="top">
                 <td class="indexname" id="indexmenuleft" align="left" valign="top"><?= $title ?></td>
                 <td id="indexmenuright" align="right">&nbsp;
-                <a onkeydownset="1" href="login.php"><?php
+                <a onkeydownset="0" href="login.php"><?php
         if ($auth->IsAuthenticated()) {
             foreach ($auth->userIDs as $userID) {
                 ?><img src="<?=getImageURL($userID, new ImageParams(self::USERIMAGESIZE, self::USERIMAGESIZE), null, 'Users') ?>" width="<?= self::USERIMAGESIZE ?>" height="<?= self::USERIMAGESIZE ?>" /><?php
